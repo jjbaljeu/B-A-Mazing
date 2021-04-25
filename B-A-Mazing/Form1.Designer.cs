021c2fb3e5b6c9b2428686cf31127d38c8f7ea64
jjbaljeu <jan.baljeu@pracsens.nl>
2021-04-25 18:10:09 +0200

    Initial

---
+++ B-A-Mazing/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace B_A_Mazing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadImage = new System.Windows.Forms.Button();
+            this.buttonAMaze = new System.Windows.Forms.Button();
+            this.buttonSaveImage = new System.Windows.Forms.Button();
+            this.pictureBoxMazeInput = new System.Windows.Forms.PictureBox();
+            this.numericUpDownPrunning = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBoxMessages = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMazeInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrunning)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLoadImage
+            // 
+            this.buttonLoadImage.Location = new System.Drawing.Point(48, 48);
+            this.buttonLoadImage.Name = "buttonLoadImage";
+            this.buttonLoadImage.Size = new System.Drawing.Size(176, 47);
+            this.buttonLoadImage.TabIndex = 0;
+            this.buttonLoadImage.Text = "Load Image";
+            this.buttonLoadImage.UseVisualStyleBackColor = true;
+            this.buttonLoadImage.Click += new System.EventHandler(this.buttonLoadImage_Click);
+            // 
+            // buttonAMaze
+            // 
+            this.buttonAMaze.Enabled = false;
+            this.buttonAMaze.Location = new System.Drawing.Point(307, 48);
+            this.buttonAMaze.Name = "buttonAMaze";
+            this.buttonAMaze.Size = new System.Drawing.Size(190, 47);
+            this.buttonAMaze.TabIndex = 1;
+            this.buttonAMaze.Text = "A-Maze";
+            this.buttonAMaze.UseVisualStyleBackColor = true;
+            this.buttonAMaze.Click += new System.EventHandler(this.buttonAMaze_Click);
+            // 
+            // buttonSaveImage
+            // 
+            this.buttonSaveImage.Enabled = false;
+            this.buttonSaveImage.Location = new System.Drawing.Point(571, 48);
+            this.buttonSaveImage.Name = "buttonSaveImage";
+            this.buttonSaveImage.Size = new System.Drawing.Size(179, 47);
+            this.buttonSaveImage.TabIndex = 2;
+            this.buttonSaveImage.Text = "Save Image";
+            this.buttonSaveImage.UseVisualStyleBackColor = true;
+            this.buttonSaveImage.Click += new System.EventHandler(this.buttonSaveImage_Click);
+            // 
+            // pictureBoxMazeInput
+            // 
+            this.pictureBoxMazeInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxMazeInput.Location = new System.Drawing.Point(48, 131);
+            this.pictureBoxMazeInput.Name = "pictureBoxMazeInput";
+            this.pictureBoxMazeInput.Size = new System.Drawing.Size(335, 250);
+            this.pictureBoxMazeInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxMazeInput.TabIndex = 3;
+            this.pictureBoxMazeInput.TabStop = false;
+            // 
+            // numericUpDownPrunning
+            // 
+            this.numericUpDownPrunning.Location = new System.Drawing.Point(649, 131);
+            this.numericUpDownPrunning.Name = "numericUpDownPrunning";
+            this.numericUpDownPrunning.Size = new System.Drawing.Size(101, 31);
+            this.numericUpDownPrunning.TabIndex = 4;
+            this.numericUpDownPrunning.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            this.numericUpDownPrunning.ValueChanged += new System.EventHandler(this.numericUpDownPrunning_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(488, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Prunning Passes";
+            // 
+            // richTextBoxMessages
+            // 
+            this.richTextBoxMessages.Location = new System.Drawing.Point(434, 190);
+            this.richTextBoxMessages.Name = "richTextBoxMessages";
+            this.richTextBoxMessages.Size = new System.Drawing.Size(316, 191);
+            this.richTextBoxMessages.TabIndex = 6;
+            this.richTextBoxMessages.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBoxMessages);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDownPrunning);
+            this.Controls.Add(this.pictureBoxMazeInput);
+            this.Controls.Add(this.buttonSaveImage);
+            this.Controls.Add(this.buttonAMaze);
+            this.Controls.Add(this.buttonLoadImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMazeInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrunning)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadImage;
+        private System.Windows.Forms.Button buttonAMaze;
+        private System.Windows.Forms.Button buttonSaveImage;
+        private System.Windows.Forms.PictureBox pictureBoxMazeInput;
+        private System.Windows.Forms.NumericUpDown numericUpDownPrunning;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBoxMessages;
+    }
+}
+
---
+++ B-A-Mazing/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B_A_Mazing
+{
+    public partial class Form1 : Form
+    {
+        Bitmap InputImage, AMazeImage;
+
+        public struct Position
+        {
+            private int x;
+            private int y;
+
+            public int X
+            {
+                get { return x; }
+                set { x = value; }
+            }
+            public int Y
+            {
+                get { return y; }
+                set { y = value; }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSaveImage_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.CreatePrompt = true;
+                saveFileDialog.DefaultExt = "jpg";
+                saveFileDialog.Filter = "File Images (*.jpg;*.jpeg;) | *.jpg;*.jpeg; |PNG Images | *.png |GIF Images | *.GIF";
+                saveFileDialog.InitialDirectory = "c:\\gegevens\\prive\\tekenen\\";
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        AMazeImage.Save(saveFileDialog.FileName);
+                        buttonSaveImage.Enabled = false;
+                    }
+                    catch (ArgumentException)
+                    {
+                        MessageBox.Show("There was an error." +
+                            "File could not be saved.");
+                    }
+                }
+            }
+        }
+
+        private void buttonAMaze_Click(object sender, EventArgs e)
+        {
+            Position CurrentPosition = new Position();
+            int NoOfExits, NoOfPasses, MaxPasses;
+            bool DeadEndsFound;
+            DateTime StartTimePrunning = new DateTime();
+            DateTime EndTimePrunning = new DateTime();
+
+            richTextBoxMessages.Text = richTextBoxMessages.Text + "\n------------------";
+
+            MaxPasses = (int)numericUpDownPrunning.Value;
+            if (MaxPasses > 0)
+            {
+                NoOfPasses = 0;
+                StartTimePrunning = DateTime.Now;
+                do
+                {
+                    DeadEndsFound = false;
+                    for (CurrentPosition.Y = 1; CurrentPosition.Y <= AMazeImage.Height - 2; CurrentPosition.Y++)
+                    {
+                        for (CurrentPosition.X = 1; CurrentPosition.X <= AMazeImage.Width - 2; CurrentPosition.X++)
+                        {
+                            if (AMazeImage.GetPixel(CurrentPosition.X, CurrentPosition.Y).R == 255)
+                            {
+                                NoOfExits = 4;
+                                if (AMazeImage.GetPixel(CurrentPosition.X, CurrentPosition.Y - 1).R == 0) { NoOfExits--; };
+                                if (AMazeImage.GetPixel(CurrentPosition.X, CurrentPosition.Y + 1).R == 0) { NoOfExits--; };
+                                if (AMazeImage.GetPixel(CurrentPosition.X - 1, CurrentPosition.Y).R == 0) { NoOfExits--; };
+                                if (AMazeImage.GetPixel(CurrentPosition.X + 1, CurrentPosition.Y).R == 0) { NoOfExits--; };
+                                if (NoOfExits <= 1)
+                                {
+                                    AMazeImage.SetPixel(CurrentPosition.X, CurrentPosition.Y, Color.FromArgb(0, 0, 128));
+                                    DeadEndsFound = true;
+                                }
+                            }
+                        }
+                    }
+                    NoOfPasses = NoOfPasses + 1;
+                } while (DeadEndsFound && (NoOfPasses < MaxPasses));
+                EndTimePrunning = DateTime.Now;
+                richTextBoxMessages.Text = richTextBoxMessages.Text + "\nPrunning passes: " + NoOfPasses;
+                if (NoOfPasses == MaxPasses) { richTextBoxMessages.Text = richTextBoxMessages.Text + " (limited)"; };
+                richTextBoxMessages.Text = richTextBoxMessages.Text + "\nPrunning time: \t" + (EndTimePrunning - StartTimePrunning).TotalMilliseconds + " mSec";
+            }
+            else
+            {
+                richTextBoxMessages.Text = richTextBoxMessages.Text + "\nNo Prunning"; 
+            }
+
+            Position StartPositionMaze = new Position();
+            Position[] Path = new Position[2];
+            Position[] PathNew;
+            List<Position[]> PathList = new List<Position[]>();
+
+            //Locate the startposition in the top row
+            CurrentPosition.Y = 0;
+            for (CurrentPosition.X = 1; CurrentPosition.X <= AMazeImage.Width - 2; CurrentPosition.X++)
+            {
+                if (AMazeImage.GetPixel(CurrentPosition.X, CurrentPosition.Y).R == 255)
+                {
+                    StartPositionMaze = CurrentPosition;
+                    Path[0] = StartPositionMaze;
+                    AMazeImage.SetPixel(StartPositionMaze.X, StartPositionMaze.Y, Color.FromArgb(0, 0, 255));
+                    CurrentPosition.X = StartPositionMaze.X;
+                    CurrentPosition.Y = StartPositionMaze.Y + 1;
+                    Path[1] = CurrentPosition;
+                    PathList.Add(Path);
+                    break;
+                }
+            }
+            if (PathList.Count == 0) { richTextBoxMessages.Text = richTextBoxMessages.Text + "\nNo start position found."; };
+
+            DateTime StartTimeSolving, EndTimeSolving;
+            StartTimeSolving = DateTime.Now;
+
+            while (PathList.Count() > 0)
+            {
+                CurrentPosition = PathList[0][PathList[0].Length - 1];
+
+                //Mark current position as 'no exit' i.e. path not available since it's already part of a path
+                AMazeImage.SetPixel(CurrentPosition.X, CurrentPosition.Y, Color.FromArgb(0, 0, 255));
+
+                //Scan surrounding for available exits i.e. possible paths if found then add this as a 
+                //new path to the list
+
+                //Check down position first since it's the only possiblity for an exit that ends the maze
+                if (AMazeImage.GetPixel(CurrentPosition.X, CurrentPosition.Y + 1).R == 255)
+                {
+                    PathNew = PathList[0];
+                    Array.Resize(ref PathNew, PathNew.Length + 1);
+                    PathNew[PathNew.Length - 1].X = CurrentPosition.X;
+                    PathNew[PathNew.Length - 1].Y = CurrentPosition.Y + 1;
+                    PathList.Add(PathNew);
+
+                    //Check if the path ends at the exit (bottom row), if so quit checking
+                    if (CurrentPosition.Y + 1 == AMazeImage.Height - 1) { break; };
+                }
+
+                //Check up position
+                if (AMazeImage.GetPixel(CurrentPosition.X, CurrentPosition.Y - 1).R == 255)
+                {
+                    PathNew = PathList[0];
+                    Array.Resize(ref PathNew, PathNew.Length + 1);
+                    PathNew[PathNew.Length - 1].X = CurrentPosition.X;
+                    PathNew[PathNew.Length - 1].Y = CurrentPosition.Y - 1;
+                    PathList.Add(PathNew);
+                }
+
+                //Check left position
+                if (AMazeImage.GetPixel(CurrentPosition.X - 1, CurrentPosition.Y).R == 255)
+                {
+                    PathNew = PathList[0];
+                    Array.Resize(ref PathNew, PathNew.Length + 1);
+                    PathNew[PathNew.Length - 1].X = CurrentPosition.X - 1;
+                    PathNew[PathNew.Length - 1].Y = CurrentPosition.Y;
+                    PathList.Add(PathNew);
+                }
+                
+                //Check right position
+                if (AMazeImage.GetPixel(CurrentPosition.X + 1, CurrentPosition.Y).R == 255)
+                {
+                    PathNew = PathList[0];
+                    Array.Resize(ref PathNew, PathNew.Length + 1);
+                    PathNew[PathNew.Length - 1].X = CurrentPosition.X + 1;
+                    PathNew[PathNew.Length - 1].Y = CurrentPosition.Y;
+                    PathList.Add(PathNew);
+                }
+
+                //This path has been processed so remove it from the list
+                PathList.RemoveAt(0);
+            }
+
+            EndTimeSolving = DateTime.Now;
+            richTextBoxMessages.Text = richTextBoxMessages.Text + "\nSolving time: \t" + (EndTimeSolving - StartTimeSolving).TotalMilliseconds + " mSec";
+            richTextBoxMessages.Text = richTextBoxMessages.Text + "\nTotal time: \t" + ((EndTimePrunning - StartTimePrunning) + (EndTimeSolving - StartTimeSolving)).TotalMilliseconds + " mSec";
+
+            //The last path in the list contains the shortest route, color this path green
+            if (PathList.Count() > 0)
+            {
+                for (int PathCounter = 0; PathCounter <= PathList.Last().Count() - 1; PathCounter++)
+                {
+                    CurrentPosition = PathList.Last()[PathCounter];
+                    AMazeImage.SetPixel(CurrentPosition.X, CurrentPosition.Y, Color.FromArgb(0, 255, 0));
+                }
+            }
+            else
+            {
+                richTextBoxMessages.Text = richTextBoxMessages.Text + "\nNo end position found.";
+            }
+
+            //Update the screen and enable the option to save the solution
+            pictureBoxMazeInput.Refresh();
+            buttonSaveImage.Enabled = true;
+            buttonAMaze.Enabled = false;
+        }
+
+        private void numericUpDownPrunning_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLoadImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\gegevens\\prive\\tekenen\\";
+                openFileDialog.Filter = "PNG|*.png|JPEG|*.jpg|Bitmap|*.bmp";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        InputImage = new Bitmap(@openFileDialog.FileName, true);
+                        AMazeImage = new Bitmap(InputImage.Width, InputImage.Height);
+
+                        int CurrentX, CurrentY;
+
+                        for (CurrentX = 0; CurrentX < InputImage.Width; CurrentX++)
+                        {
+                            for (CurrentY = 0; CurrentY < InputImage.Height; CurrentY++)
+                            {
+                                AMazeImage.SetPixel(CurrentX, CurrentY, InputImage.GetPixel(CurrentX, CurrentY));
+                            }
+                        }
+
+                        pictureBoxMazeInput.Image = AMazeImage;
+                        buttonSaveImage.Enabled = false;
+                        buttonAMaze.Enabled = true;
+                    }
+                    catch (ArgumentException)
+                    {
+                        MessageBox.Show("There was an error." +
+                            "Check the path to the image file.");
+                    }
+                }
+            }
+
+
+
+
+        }
+    }
+}
